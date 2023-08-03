85494a9238bf864741e90bef1e878030a6038b72
Sravan SP <sravansp92@gmail.com>
2023-08-03 15:09:02 +0530

    changes in aboutus all apsge top header navbar m-w

--- assets/js/aboutus.js
+++ assets/js/aboutus.js
@@ -274,7 +274,7 @@ function aboutAnimation() {
                 opacity: 0,
                 duration: 1,
                 ease: "power3.out",
-                delay: 1,
+                // delay: 1,
             })
             .from(".about_h_underline", {
                 y: 20,
