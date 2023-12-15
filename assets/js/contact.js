df07d9ea344458bd2899467f9247a50403b482e3
Sravan SP <sravansp92@gmail.com>
2023-12-15 09:57:59 +0530

    popup added by abihekh

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -6,6 +6,15 @@ function sendEmail(){
         message:document.getElementById("message").value,
     }
     emailjs.send("service_ms1hiwp","template_e7wa7ls",params).then(function(res){
-        alert('Your Email has been sent!');
+        
+    }).then(function openPopup(){
+        popup.classList.add("open-popup");
+       
     })
+    
+}
+let popup = document.getElementById("popup")
+
+function closePopup() {
+popup.classList.remove("open-popup");
 }
